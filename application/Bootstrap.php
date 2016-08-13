29c2927e19df7f774ef57b6e56d65d9abc13a935
SaraAbb <saraabbonizio@live.it>
2016-08-13 18:26:53 +0200

    Aggiornamento

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,41 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected function _initLogging()
+    {
+        $logger = new Zend_Log();
+        $writer = new Zend_Log_Writer_Firebug();
+        $logger->addWriter($writer);
 
+        Zend_Registry::set('log', $logger);
+
+        $this->_logger = $logger;
+        $this->_logger->info('Bootstrap ' . __METHOD__);
+    }
+
+    protected function _initDefaultModuleAutoloader()
+    {
+        $loader = Zend_Loader_Autoloader::getInstance();
+        $loader->registerNamespace('App_');
+        $this->getResourceLoader()
+            ->addResourceType('modelResource','models/resources','Resource');
+    }
+    protected function _initFrontControllerPlugin()
+    {
+        $front = Zend_Controller_Front::getInstance();
+        $front->registerPlugin(new App_Controller_Plugin_Acl());
+    }
+
+    protected function _initDbParms()
+    {
+        $db = new Zend_Db_Adapter_Pdo_Mysql(array(
+            'host'     => 'localhost',
+            'username' => 'admin',
+            'password' => 'password',
+            'dbname'   => 'tweb'
+        ));
+        Zend_Db_Table_Abstract::setDefaultAdapter($db);
+    }
 
 }
 
--- application/controllers/PublicController.php
+++ application/controllers/PublicController.php
@@ -1,7 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sara
- * Date: 13/08/2016
- * Time: 18:13
- */
+
+class PublicController extends Zend_Controller_Action
+{
+    protected $_publicModel;
+    protected $_auth;
+    protected $db;
+
+    public function init()
+    {
+        $this->_helper->layout->setLayout('layout');
+        $this->_logger = Zend_Registry::get('log');
+        $this->_publicModel=new Application_Model_Public();
+        $this->db=new Zend_Db_Adapter_Pdo_Mysql(array(
+            'host'     => 'localhost',
+            'username' => 'admin',
+            'password' => 'password',
+            'dbname'   => 'tweb'
+        ));
+    }
+
+    public function indexAction()
+    {
+        $view = new Zend_View();
+        $view->setScriptPath(VIEW_PATH);
+        return $view->render('index.phtml');
+    }
+
+    public function viewstaticAction () {
+        $page = $this->_getParam('staticPage');
+        $this->render($page);
+    }
+    
+    public function registratiAction() {
+        $form=new Application_Form_Public_Registra();
+        if($this->getRequest()->isPost())
+        {
+            if($form->isValid($_POST)) {
+                $dati = $form->getValues();
+                $mail=$form->getValue('email');
+                $mailgiàpresente=$this->controllaemailAction($mail);
+                if(!$mail==$mailgiàpresente)
+                {
+                    $this->_publicModel->registrati($dati);
+                    echo 'Registrazione avvenuta!';
+                }
+                else{
+                    echo 'Mail già in uso, inserirne una diversa';
+                }
+
+            }
+            else
+            {
+                echo 'Qualcosa è andato storto';
+            }
+        }
+        $this->view->assign('form', $form);
+    }
+    
+    public function loginAction() {
+        
+    }
+
+    public function cercaUtenteAction(){
+
+    }
+
+    public function controllaemailAction($info){
+        $a=new Application_Model_User();
+        $email=$a->cercamail($info);
+        return $email;
+    }
+    
+}
