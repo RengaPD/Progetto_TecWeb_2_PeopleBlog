b5427055a2108341b896bd7a063edf3b633de40b
RengaPD <falomen90@gmail.com>
2016-08-28 01:48:41 +0200

    Gestione delle Amicizie 1
    create funzioni di base nel file Users.php
    Creati file e cartelle necessari

--- application/models/User.php
+++ application/models/User.php
@@ -19,4 +19,20 @@ class Application_Model_User extends App_Model_Abstract
        $this->getResource('Blog')->creaBlog($dati);
        $this->getResource('Utenti')->setBlogtrue(); 
     }
+    public function aggiungiamico($id_user)
+    {
+        return $this->getResource('Amici')->addfriend($id_user);
+    }
+    public function rimuoviamco($id_user)
+    {
+        return $this->getResource('Amici')->remfriend($id_user);
+    }
+    public function accettarichiesta($id_request)
+    {
+        return $this->getResource('Amici')->acceptfriend($id_request);
+    }
+    public function rifiutarichiesta($id_request)
+    {
+        return $this->getResource('Amici')->refusefriend($id_request);
+    }
 }
